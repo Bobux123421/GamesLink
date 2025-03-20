c65e22894af6d00f44ccca754c9fb3d2e2cdc9c3
Bobux123421 <jovan@jovhub.xyz>
2025-03-20 16:03:29 +0700

    Update Loader.lua

--- Loader.lua
+++ Loader.lua
@@ -1,1 +1,9 @@
-print("hi")
+local currentPlaceId = game.PlaceId
+
+
+if currentPlaceId == 6137321701 or currentPlaceId == 2239430935 then
+    print("Detected supported game! Loading script...")
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/Bobux123421/GamesLink/refs/heads/main/Blair.lua"))()
+else
+    print("Not in a supported game. Current PlaceId: " .. tostring(currentPlaceId))
+end 
