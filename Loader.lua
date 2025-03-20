b5592c6d2f6f3297130d2e07278f25f554edafca
Bobux123421 <jovan@jovhub.xyz>
2025-03-20 16:20:38 +0700

    Update Loader.lua

--- Loader.lua
+++ Loader.lua
@@ -1,9 +1,14 @@
 local currentPlaceId = game.PlaceId
 
-
 if currentPlaceId == 6137321701 or currentPlaceId == 2239430935 then
-    print("Detected supported game! Loading script...")
+    print("Detected Blair! Loading script...")
     loadstring(game:HttpGet("https://raw.githubusercontent.com/Bobux123421/GamesLink/refs/heads/main/Blair.lua"))()
+elseif currentPlaceId == 6170143659 then
+    print("Detected D3M0N0L0GY! Loading script...")
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/Bobux123421/GamesLink/refs/heads/main/D3M0N0L0GY"))()
+elseif currentPlaceId == 8260276694 then
+    print("Detected Ability Wars! Loading script...")
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/Bobux123421/GamesLink/refs/heads/main/Ability%20wars.lua"))()
 else
     print("Not in a supported game. Current PlaceId: " .. tostring(currentPlaceId))
 end 
